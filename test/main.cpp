7169c36609daf4cbd82585ca5ec2af6c235ab6cf
choidaruhan <chl11wq12@gmail.com>
2018-12-14 00:28:34 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 최다루한 on 14/12/2018.
+//  Copyright © 2018 최다루한. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
